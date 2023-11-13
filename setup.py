6bbd611498d7051e67e63890dbbd79264671f15d
Simon0207e <117093549+Simon0207e@users.noreply.github.com>
2023-11-13 22:03:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='math-quiz',
-    version='0.1.0',
+    version='0.1.dev6+g7fd7d2f',
     license='MIT',
     packages=find_packages(),
     install_requires=[
