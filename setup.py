0afd5b45362ad90b4a130e0e4934d4df5563c5f8
Simon0207e <117093549+Simon0207e@users.noreply.github.com>
2023-11-13 21:59:42 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math-quiz',
+    version='0.1.0',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'setuptools==68.2.2',
+        'wheel==0.41.3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'math-quiz = math_quiz.module:main_function',
+        ],
+    },
+    author='Ximeng Zhang',
+    author_email='zximeng0203@gmail.com',
+    description=' math quiz project',
+    long_description='',
+    url='https://github.com/Simon0207e/dsss_homework_2.git',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    use_scm_version=True,
+)
